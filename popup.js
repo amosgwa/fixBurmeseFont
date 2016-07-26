c18a9956f85aaa5506ab719e374565c140204294
Amos Gwa <amosgwa@gmail.com>
2016-07-25 19:31:48 -0600

    It now works on https websites

--- popup.js
+++ popup.js
@@ -4,10 +4,13 @@ function fixit() {
         code: "var newStyle = document.createElement('style');\
                 newStyle.appendChild(document.createTextNode(\"\
                   @font-face{\
-                    font-family:Zawgyi-One;\
-                    src:local('Zawgyi-One'),url('http://cdn.myanmapress.com/fonts/zawgyi.ttf');\
+                    font-family:Zawgyi-One !important;\
+                    src:local('Zawgyi-One'),url('https://cdn.myanmapress.com/fonts/zawgyi.ttf');\
                   }\
-                \")); document.head.appendChild(newStyle); document.body.style.fontFamily='Zawgyi-One'; console.log(newStyle);"
+                  div, span, p, h1, h2, h3, h4, h5, h6, body, section, article, form, pre, a {\
+                    font-family:Zawgyi-One !important;\
+                  }\
+                \")); document.head.appendChild(newStyle);"
     });
     window.close();
 };
